1351169509a08ffd3e1c433c2a0e4c1df84bd529
tibble <info@talmage.dev>
2026-01-28 08:47:37 -0700

    feat: add alias

--- config/.bashrc.d/alias.bash
+++ config/.bashrc.d/alias.bash
@@ -4,3 +4,6 @@ alias t='tmux attach || tmux'
 
 alias ..='cd ..'
 alias ...='cd ../..'
+
+# Fix beads socket for macOS bind mount
+alias beads-link='mkdir -p /run/beads && ln -sf /run/beads/bd.sock /workspace/.beads/bd.sock'
